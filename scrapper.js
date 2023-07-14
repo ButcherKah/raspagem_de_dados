361d656fa0965c74e19941e0f6780a110b0cb457
Karina Marra <margarido.marra.karina@gmail.com>
2023-07-14 16:01:51 -0300

    adicionou credenciais a arquivo json.

--- scrapper.js
+++ scrapper.js
@@ -1,16 +1,34 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const fs = require('fs');
+const { json } = require('stream/consumers');
 
 async function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function readJSONFile(filePath) {
+    try {
+      const fileContent = fs.readFileSync(filePath, 'utf-8');
+      const jsonData = JSON.parse(fileContent);
+      const username = jsonData.username;
+      const password = jsonData.password;
+      const url = jsonData.url;
+      const term = jsonData.term;
+      return { username, password, url, term };
+    } catch (error) {
+      console.error('Error reading JSON file:', error);
+      return null;
+    }
+  }
+
 async function performScrapper() {
+    const filePath = './settings.json';
+    const jsonData = readJSONFile(filePath);
     var content = "";
-    let url = "test.com";
-    let term = "test";
-    let accountEmail = "";
-    let accountPassword = "";
+    let url = jsonData.url;
+    let term = jsonData.term;
+    let accountEmail = jsonData.username;
+    let accountPassword = jsonData.password;
     const driver = await new Builder().forBrowser('chrome').build();
     console.log("************************************************")
     console.log("Scrapper Inicializado.")
